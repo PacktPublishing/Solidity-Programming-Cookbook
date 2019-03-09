fbf4d308756a5de644c2a135487c60a4d06c6cad
Raj Jha <rajkrjha@hotmail.com>
2019-03-10 01:21:04 +0530

    Update 8_value_type_address_2.sol

--- Chapter 1/solidity_programming_cookbook_samples/8_value_type_address_2.sol
+++ Chapter 1/solidity_programming_cookbook_samples/8_value_type_address_2.sol
@@ -1,6 +1,9 @@
+/*
+    @AUTHOR - Raj Jha & Harish Jaggi
+*/
 pragma solidity ^0.5.1;  
 contract FundTransferer {  
-    function Fundtransferer(address payable _address, uint amount) public {  
+    function Fundtransferer(address payable _address, uint amount) public { //try argument (0x14723a09acff6d2a60dcdf7aa4aff308fddc160c,100)
         address myAddress = address(this); //contract address
         if (_address.balance < 10 && myAddress.balance >= 10) { //check balance of sender and receiver(contract address) and then transfer to contract address.
         _address.transfer(amount);
