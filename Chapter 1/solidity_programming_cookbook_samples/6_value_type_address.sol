969e705d4c6985dedc388c2df24b8f19a97e0fa6
Raj Jha <rajkrjha@hotmail.com>
2019-03-10 01:18:39 +0530

    Update 6_value_type_address.sol

--- Chapter 1/solidity_programming_cookbook_samples/6_value_type_address.sol
+++ Chapter 1/solidity_programming_cookbook_samples/6_value_type_address.sol
@@ -1,10 +1,14 @@
-
+/*
+    @AUTHOR - Raj Jha & Harish Jaggi
+*/
 pragma solidity ^0.5.1;  
 contract AddressGetter {  
-    function getCallerAddresses() public view returns (address caller, address contract1) {  
+    function getCallerAddresses() public view returns (address caller, address contract1) {  //no arguments needed.
         caller = msg.sender;  //get caller address
         contract1 = address(this); //get contract address  
         return (caller,contract1);
     }  
 }
+
+
 
