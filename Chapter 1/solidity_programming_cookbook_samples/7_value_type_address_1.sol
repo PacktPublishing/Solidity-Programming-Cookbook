9dac7c7393c115a82ffafa99a61f5dd98f71e22d
Raj Jha <rajkrjha@hotmail.com>
2019-03-10 01:17:43 +0530

    Update 7_value_type_address_1.sol

--- Chapter 1/solidity_programming_cookbook_samples/7_value_type_address_1.sol
+++ Chapter 1/solidity_programming_cookbook_samples/7_value_type_address_1.sol
@@ -1,12 +1,15 @@
+/*
+    @AUTHOR - Raj Jha & Harish Jaggi
+*/
 pragma solidity ^0.5.1;  
   
 contract FundTransferer {  
       
-    function Fundtransferer(address payable _address, uint amount) public {  
-        _address.transfer(amount);  //use of transfer is prefferd because throws an exception and stop the execution
+    function Fundtransferer(address payable _address, uint amount) public {  //try argument (0x14723a09acff6d2a60dcdf7aa4aff308fddc160c,100)
+        _address.transfer(amount);  //use of transfer is preferred because throws an exception and stop the execution
     }  
       
-    function FundSender(address payable _address, uint amount) public returns(bool){  
+    function FundSender(address payable _address, uint amount) public returns(bool){ //try argument (0x14723a09acff6d2a60dcdf7aa4aff308fddc160c,100)  
         _address.send(amount);  //send returns false on error.
     }  
 }  
