8cb5cf2b8d4532a1915dcc2b4c687e8e3ce8f676
Raj Jha <rajkrjha@hotmail.com>
2019-03-23 21:23:13 +0530

    Update 20_function_types_public_internal.sol

--- Chapter 1/solidity_programming_cookbook_samples/20_function_types_public_internal.sol
+++ Chapter 1/solidity_programming_cookbook_samples/20_function_types_public_internal.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.1;
 contract Math {
-    /* Anyone can call the function, It is part of contract interface and can be either called internally or via messages. 
+    /* Anyone can call the function, being part of contract interface it is callable internally or through messages.  
     An automatic getter function is generated For public state variables.*/
     function getFactorial(uint number) public pure returns( uint ){
         return factorial(number); //call of internal function.
